a9007e8113cf7417f53b3a066c3de3bc0406414d
Cihan <cihankurt1998@gmail.com>
2020-01-22 15:57:10 +0100

    Player Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    public LayerMask movementMask;
+
+    Camera cam;
+    PlayerMotor motor;
+    void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // Left mouse
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                motor.MoveToPoint(hit.point);
+            }
+        }
+        if (Input.GetMouseButtonDown(1)) // Right mouse
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                
+            }
+        }
+    }
+}
