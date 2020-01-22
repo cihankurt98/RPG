5415f7f8089182d26bbcef5d43f6886cbf3ba07a
Cihan <cihankurt1998@gmail.com>
2020-01-22 16:23:35 +0100

    Interaction basics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public LayerMask movementMask;
-
+    public Interactable focus;
     Camera cam;
     PlayerMotor motor;
     void Start()
@@ -25,6 +25,7 @@ public class PlayerController : MonoBehaviour
             if (Physics.Raycast(ray, out hit, 100, movementMask))
             {
                 motor.MoveToPoint(hit.point);
+                RemoveFocus();
             }
         }
         if (Input.GetMouseButtonDown(1)) // Right mouse
@@ -34,8 +35,38 @@ public class PlayerController : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit, 100))
             {
-                
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                if (interactable != null)
+                {
+                    SetFocus(interactable);
+                }
             }
         }
+    }
+
+    void SetFocus(Interactable newFocus)
+    {
+        if (newFocus != focus)
+        {
+            if (focus != null)
+            {
+                focus.OnDefocused();
+            }
+            focus = newFocus;
+            motor.FollowTarget(newFocus);
+
+        }
+        newFocus.OnFocused(transform);
+
+    }
+
+    void RemoveFocus()
+    {
+        if (focus != null)
+        {
+            focus.OnDefocused();
+        }
+        focus = null;
+        motor.StopFollowingTarget();
     }
 }
